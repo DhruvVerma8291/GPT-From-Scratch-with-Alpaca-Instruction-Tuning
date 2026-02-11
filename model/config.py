7d3bf304a74ac31edca49a4143b1d144d4e94091
DhruvVerma8291 <dhruvverma2898@gmail.com>
2026-02-11 20:50:50 +0530

    Create config.py

---
+++ model/config.py
@@ -0,0 +1,9 @@
+BASE_CONFIG = {
+    "vocab_size": 50257,     # Vocabulary size
+    "context_length": 1024,  # Context length
+    "drop_rate": 0.0,        # Dropout rate
+    "qkv_bias": True,         # Query-key-value bias
+    "emb_dim": 1024, 
+    "n_layers": 24, 
+    "n_heads": 16
+}
